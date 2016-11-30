204b143a3c759926fb1307f8b460b51b346f3157
MINHHIEU\minhh <hieutm.bk@gmail.com>
2016-11-30 22:16:24 +0700

    Imitial Commit

---
+++ 1945/src/Program.java
@@ -0,0 +1,9 @@
+/**
+ * Created by minhh on 30/11/2016.
+ */
+public class Program {
+    public static void main(String args[]){
+        System.out.println("Hello Android 7");
+        GameWindow gameWindow = new GameWindow();
+    }
+}
